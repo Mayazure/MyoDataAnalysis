c6c2dffe4c423a934f66c9a285cc19f64eb4a09f
Mayazure <mayazureee@gmail.com>
2017-10-14 15:06:41 +1100

    Update.

--- main.m
+++ main.m
@@ -6,7 +6,7 @@ density = 3;
 %for i=2:4
     for d=0:2
         %extractFeaturesFromDir(strcat('D:\Myo\Organized Data\class\',num2str(i),'\ParsedData_',num2str(density),'\',num2str(d),'\'),d);
-        extractFeaturesFromDir(strcat('H:\Myo\Data\Processing\ParsedData_3\',num2str(d),'\'),d);
+        extractFeaturesFromDir(strcat('H:\Myo\Data\Processing\ParsedData_3_anger\',num2str(d),'\'),d);
     end
 %end
 
